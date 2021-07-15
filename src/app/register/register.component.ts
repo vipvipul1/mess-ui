51d6abf7e5141a4712b2cf19fa3860ed3d0fc445
Vipul Kumar <vipvipul1@gmail.com>
2021-07-15 16:46:49 +0530

    user registration completed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from '../app-model/user.model';
+import { UserRegisterService } from '../app-service/authentication/user-register.service';
 
 @Component({
   selector: 'app-register',
@@ -8,18 +10,45 @@ import { User } from '../app-model/user.model';
 })
 export class RegisterComponent implements OnInit {
 
-  errorMessage = '';
+  errorMsg = '';
+  successMsg = '';
+  emailErr: boolean = false;
+  usernameErr = false;
+  phoneErr = false;
+  studentIdErr = false;
   user = new User();
 
   constructor(
+    private userRegister: UserRegisterService
   ) { }
 
   ngOnInit() {
     this.user.isVeg = false;
   }
 
+  validateRegDetails(userDetail) {
+    this.userRegister.validateRegDetails(userDetail).subscribe(
+      (res) => {
+        return res;
+      }, (err) => {
+        this.errorMsg = err.error.friendlyMessage;
+        return true;
+      }
+    );
+  }
+
   registerUser() {
-    
+    this.user.userId = -1;
+    this.userRegister.registerNewUser(this.user).subscribe(
+      (res) => {
+        this.successMsg = 'User registered successfully!';
+        this.errorMsg = '';
+        this.user = new User();
+      }, (err) => {
+        this.errorMsg = 'User registration failed!\n' + err.error.friendlyMessage;
+        this.successMsg = '';
+      }
+    );
   }
 
 }
